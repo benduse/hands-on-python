42b6e2fc735d98aeb143fc41ebf5a685c8c17389
benduse <110694311+benduse@users.noreply.github.com>
2026-08-20 10:49:07 +0000

    slicing and reversing lists 0203

--- Ex_Files/02_03_begin/main.py
+++ Ex_Files/02_03_begin/main.py
@@ -4,15 +4,16 @@ AGES = [20, 21, 22, 23]
 JOHN = NAMES[0]
 PAUL = NAMES[1]
 
-JOHN_PAUL = NAMES[:2]
-GEORGE_RINGO = NAMES[2:]
-REVERSE = NAMES[::-1]
-EVERY_OTHER = NAMES[::2]
+JOHN_PAUL = NAMES[:2] #slicing name to the left of two
+GEORGE_RINGO = NAMES[2:4] #slicing name from index 2 to 4
+REVERSE = NAMES[::-1] #reversing the list
+EVERY_OTHER = NAMES[::2] #taking every other element
 
-print(sum(AGES))
-print(min(AGES))
-print(max(AGES))
+print("this is the sum: " + str(sum(AGES)))
+print("this is the minimum: " + str(min(AGES)))
+print("this is the maximum: " + str(max(AGES)))
 
 print(JOHN_PAUL)
 print(GEORGE_RINGO)
-print(REVERSE)
+print("this is the reverse: " + str(REVERSE))
+print("this is every other: " + str(EVERY_OTHER))
